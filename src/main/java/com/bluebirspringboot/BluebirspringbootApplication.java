103a3bf7eefd60eb6cf34cbfd626dfdad6dc44be
pradeepyadav11895 <er.pradeepy118@gmail.com>
2018-05-25 13:24:07 +0530

    initail commit for spring boot project

---
+++ src/main/java/com/bluebirspringboot/BluebirspringbootApplication.java
@@ -0,0 +1,12 @@
+package com.bluebirspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BluebirspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BluebirspringbootApplication.class, args);
+	}
+}
---
+++ src/test/java/com/bluebirspringboot/BluebirspringbootApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bluebirspringboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BluebirspringbootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
